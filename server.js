0924b8f3b82fca3a863c9a2c4b261f1a775b6ced
JoyBoyCr7 <Rnyongkah@gmail.com>
2023-11-04 01:03:37 -0500

    origin

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const DATABASE_URL = process.env.DATABASE_URL;
 console.log(" Running");
 if (process.env.ENVIRONMENT === "production") {
     app.use(cors({
-        origin: "https://jobby-job-front-ny6u-3s7rlbqtk-joyboycr7.vercel.app",
+        origin: "https://jobby-jobfrontpt2.onrender.com",
         credentials: true,
     }));
 }
